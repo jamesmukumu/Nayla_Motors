ebc6ea5a6da44ee15db61ff8d390d490b364e18f
jamesmukumu <jamesmukumu03@gmail.com>
2025-01-01 22:24:41 +0300

    det

--- src/app/allcars/allcars.service.ts
+++ src/app/allcars/allcars.service.ts
@@ -4,7 +4,7 @@ export class AllcarsService {
   car: any[] = [];
 
   async fetchCardata(): Promise<any[]> {
-    var resp = await axios.get('http://localhost:7800/fetch/all');
+    var resp = await axios.get('https://carshop-1.onrender.com/fetch/all');
     var { count, data } = resp.data;
 
     return data;
